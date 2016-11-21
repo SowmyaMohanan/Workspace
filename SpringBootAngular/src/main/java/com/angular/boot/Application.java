735acab04b30f52fbab0e4a2dbdc7e67e07ce907
475383 <475383@PC191913.cts.com>
2016-11-21 16:33:09 +0530

    modified

--- SpringBootAngular/src/main/java/com/angular/boot/Application.java
+++ SpringBootAngular/src/main/java/com/angular/boot/Application.java
@@ -1,14 +1,6 @@
 package com.angular.boot;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Inherited;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
-
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
